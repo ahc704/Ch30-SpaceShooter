3fef4dea22a2cac5a04aa235cdf10bc2ac6fd452
ahc704 <46978022+ahc704@users.noreply.github.com>
2019-10-24 22:30:49 -0400

    Setting up Space shooter

---
+++ Assets/__Script/BoundsCheck.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoundsCheck : MonoBehaviour
+{
+    [Header("Set in Inspector")]
+    public float radius = 1f;
+
+    [Header("Set Dynamicallly")]
+    public float camWidth;
+    public float camHeight;
+
+    void Awake()
+    {
+        camHeight = Camera.main.orthographicSize;
+        camWidth = camHeight * Camera.main.aspect;
+    }
+
+    
+    void LateUpdate()
+    {
+        
+    }
+}
